668b20985dc107500065b7cf84b8dcf61632b841
urko villanueva <urko.informatica@gmail.com>
2016-07-20 12:00:42 +0200

    Corregido archivo sql.

--- gestioncursos/src/main/resources/gestioncursos.sql
+++ gestioncursos/src/main/resources/gestioncursos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-07-2016 a las 10:48:43
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.5.37
+-- Tiempo de generación: 20-07-2016 a las 09:58:55
+-- Versión del servidor: 5.7.9
+-- Versión de PHP: 5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,46 +19,59 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `gestioncursos`
 --
+DROP DATABASE `gestioncursos`;
+CREATE DATABASE IF NOT EXISTS `gestioncursos` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `gestioncursos`;
 
 DELIMITER $$
 --
 -- Procedimientos
 --
+DROP PROCEDURE IF EXISTS `deleteAlumno`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `deleteAlumno` (IN `codigo` INT)  NO SQL
 DELETE FROM alumno 
 WHERE codAlumno = codigo$$
 
+DROP PROCEDURE IF EXISTS `deleteCurso`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `deleteCurso` (IN `codigo` INT)  BEGIN
 DELETE FROM curso WHERE codCurso = codigo;
 END$$
 
+DROP PROCEDURE IF EXISTS `deleteModulo`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `deleteModulo` (IN `codigo` INT)  BEGIN
 DELETE FROM modulo WHERE codModulo = codigo;
 END$$
 
+DROP PROCEDURE IF EXISTS `getAllAlumno`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getAllAlumno` ()  NO SQL
 SELECT codAlumno, a.nombre as 'nAlumno', apellidos, email, telefono, dni_nie, fNacimiento, a.codGenero, g.nombre as 'nGenero' 
 FROM alumno a
 INNER JOIN genero g ON g.codGenero = a.codGenero$$
 
+DROP PROCEDURE IF EXISTS `getAllCurso`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `getAllCurso` ()  BEGIN
 SELECT `codCurso`, `nombre` AS 'nCurso', `codPatrocinador`, `codTipoCurso` FROM `curso`;
 END$$
 
+DROP PROCEDURE IF EXISTS `getAllModulo`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `getAllModulo` ()  BEGIN
 SELECT `codModulo`, `nombre`, `uFormativa`, `duracion` FROM modulo;
 END$$
 
+DROP PROCEDURE IF EXISTS `getAlumnoById`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `getAlumnoById` (IN `codigo` INT)  NO SQL
 SELECT codAlumno, a.nombre AS 'nAlumno', apellidos, dni_nie, fNacimiento, email, telefono, a.codGenero, g.nombre AS 'nGenero' FROM alumno a INNER JOIN genero g ON g.codGenero = a.codGenero WHERE a.codAlumno = codigo$$
 
+DROP PROCEDURE IF EXISTS `getCursoById`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `getCursoById` (IN `codigo` INT)  NO SQL
 SELECT codCurso, nombre AS 'nCurso' , codPatrocinador, codTipoCurso FROM curso WHERE codCurso = codigo$$
 
+DROP PROCEDURE IF EXISTS `getModuloById`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `getModuloById` (IN `codigo` INT)  BEGIN
 SELECT `codModulo`, `nombre`, `uFormativa`, `duracion` FROM modulo WHERE codModulo = codigo;
 END$$
 
+DROP PROCEDURE IF EXISTS `insertAlumno`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertAlumno` (IN `nombre` VARCHAR(150), IN `apellidos` VARCHAR(250), IN `dni` VARCHAR(9), IN `fecha` DATE, IN `email` VARCHAR(100), IN `telefono` VARCHAR(13), IN `codGenero` INT, OUT `codAlumno` INT)  NO SQL
 BEGIN
 INSERT INTO alumno (nombre, apellidos, dni_nie, fNacimiento, email, telefono, codGenero) VALUES(LOWER(nombre), LOWER(apellidos),LOWER(dni),fecha,LOWER(email),LOWER(telefono),codGenero);
@@ -67,16 +80,19 @@ SET codAlumno = LAST_INSERT_ID();
 
 END$$
 
+DROP PROCEDURE IF EXISTS `insertCurso`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `insertCurso` (IN `nombre` VARCHAR(150), IN `codTipoCurso` INT, IN `codPatrocinador` VARCHAR(50), OUT `codCurso` INT)  BEGIN
 INSERT INTO curso(`nombre`, `codPatrocinador`, `codTipoCurso`) VALUES (LOWER(nombre), LOWER(codPatrocinador), codTipoCurso);
 SET codCurso = LAST_INSERT_ID();
 END$$
 
+DROP PROCEDURE IF EXISTS `insertModulo`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `insertModulo` (IN `nombre` VARCHAR(150), IN `uFormativa` VARCHAR(50), IN `duracion` INT, OUT `codModulo` INT)  BEGIN
 INSERT INTO modulo(`nombre`, `uFormativa`, `duracion`) VALUES (nombre,uFormativa,duracion);
 SET codModulo = LAST_INSERT_ID();
 END$$
 
+DROP PROCEDURE IF EXISTS `updateAlumno`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `updateAlumno` (IN `codigo` INT, IN `nombre` VARCHAR(150), IN `apellidos` VARCHAR(250), IN `dni` VARCHAR(9), IN `fecha` DATE, IN `email` VARCHAR(100), IN `telefono` VARCHAR(13), IN `codGenero` INT)  NO SQL
 UPDATE `alumno` SET `nombre`=LOWER(nombre),
 `apellidos`= LOWER(apellidos),
@@ -87,10 +103,12 @@ UPDATE `alumno` SET `nombre`=LOWER(nombre),
 `codGenero`=codGenero 
 WHERE codAlumno = codigo$$
 
+DROP PROCEDURE IF EXISTS `updateCurso`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `updateCurso` (IN `codigo` INT, IN `nombre` VARCHAR(150), IN `codPatrocinador` VARCHAR(50), IN `codTipo` INT)  BEGIN
 UPDATE curso SET `nombre`=nombre,`codPatrocinador`=codPatrocinador,`codTipoCurso`=codTipo WHERE codCurso = codigo;
 END$$
 
+DROP PROCEDURE IF EXISTS `updateModulo`$$
 CREATE DEFINER=`usuario`@`%` PROCEDURE `updateModulo` (IN `nombre` VARCHAR(150), IN `uFormativa` VARCHAR(50), IN `duracion` INT, IN `codigo` INT)  BEGIN
 UPDATE modulo SET `nombre`=nombre,`uFormativa`=uFormativa,`duracion`=duracion WHERE codModulo = codigo;
 END$$
@@ -103,69 +121,113 @@ DELIMITER ;
 -- Estructura de tabla para la tabla `alumno`
 --
 
-CREATE TABLE `alumno` (
-  `codAlumno` int(11) NOT NULL COMMENT 'Indice del alumno',
+DROP TABLE IF EXISTS `alumno`;
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `codAlumno` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Indice del alumno',
   `nombre` varchar(150) NOT NULL COMMENT 'Nombre del alumno',
   `apellidos` varchar(250) NOT NULL COMMENT 'Apellidos del alumno',
   `dni_nie` varchar(9) NOT NULL COMMENT 'Documento de identificacion del alumno',
   `fNacimiento` date NOT NULL COMMENT 'Fecha de nacimiento del alumno',
   `email` varchar(100) NOT NULL,
   `telefono` varchar(13) NOT NULL,
-  `codGenero` int(11) NOT NULL COMMENT 'Codigo de genero'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `codGenero` int(11) NOT NULL COMMENT 'Codigo de genero',
+  PRIMARY KEY (`codAlumno`),
+  UNIQUE KEY `dni-nie` (`dni_nie`),
+  UNIQUE KEY `codGenero_2` (`codGenero`),
+  KEY `codGenero` (`codGenero`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `alumno`
+--
+
+TRUNCATE TABLE `alumno`;
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `calificacion`
 --
 
-CREATE TABLE `calificacion` (
+DROP TABLE IF EXISTS `calificacion`;
+CREATE TABLE IF NOT EXISTS `calificacion` (
   `codAlumno` int(11) NOT NULL,
   `codCurso` int(11) NOT NULL,
   `codModulo` int(11) NOT NULL,
   `nota` int(11) NOT NULL,
-  `fExamen` date NOT NULL
+  `fExamen` date NOT NULL,
+  PRIMARY KEY (`codAlumno`,`codCurso`,`codModulo`),
+  KEY `fk_calificacion_curso` (`codCurso`),
+  KEY `fk_calificacion_modulo` (`codModulo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `calificacion`
+--
+
+TRUNCATE TABLE `calificacion`;
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `curso`
 --
 
-CREATE TABLE `curso` (
-  `codCurso` int(11) NOT NULL COMMENT 'Codigo de curso',
+DROP TABLE IF EXISTS `curso`;
+CREATE TABLE IF NOT EXISTS `curso` (
+  `codCurso` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de curso',
   `nombre` varchar(150) NOT NULL COMMENT 'Nombre del curso',
   `codPatrocinador` varchar(50) NOT NULL COMMENT 'Referencia del curso',
-  `codTipoCurso` int(11) NOT NULL COMMENT 'Codigo del tipo de curso'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `codTipoCurso` int(11) NOT NULL COMMENT 'Codigo del tipo de curso',
+  PRIMARY KEY (`codCurso`),
+  UNIQUE KEY `codTipoCurso` (`codTipoCurso`),
+  KEY `codTipoCurso_2` (`codTipoCurso`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `curso`
+--
+
+TRUNCATE TABLE `curso`;
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `curso_alumno`
 --
 
-CREATE TABLE `curso_alumno` (
+DROP TABLE IF EXISTS `curso_alumno`;
+CREATE TABLE IF NOT EXISTS `curso_alumno` (
   `codAlumno` int(11) NOT NULL COMMENT 'Codigo de alumno',
   `codCurso` int(11) NOT NULL COMMENT 'Codigo de curso',
   `referencia` varchar(50) NOT NULL,
   `fInicio` date NOT NULL COMMENT 'Fecha de inicio',
-  `fFin` date DEFAULT NULL COMMENT 'Fecha fin'
+  `fFin` date DEFAULT NULL COMMENT 'Fecha fin',
+  PRIMARY KEY (`codAlumno`,`codCurso`),
+  UNIQUE KEY `referencia` (`referencia`),
+  KEY `fk_cursoAlumno_curso` (`codCurso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `curso_alumno`
+--
+
+TRUNCATE TABLE `curso_alumno`;
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `genero`
 --
 
-CREATE TABLE `genero` (
-  `codGenero` int(11) NOT NULL COMMENT 'Codigo de genero',
-  `nombre` varchar(50) NOT NULL COMMENT 'Nombre de genero'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `genero`;
+CREATE TABLE IF NOT EXISTS `genero` (
+  `codGenero` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de genero',
+  `nombre` varchar(50) NOT NULL COMMENT 'Nombre de genero',
+  PRIMARY KEY (`codGenero`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `genero`
+--
+
+TRUNCATE TABLE `genero`;
 --
 -- Volcado de datos para la tabla `genero`
 --
@@ -181,24 +243,38 @@ INSERT INTO `genero` (`codGenero`, `nombre`) VALUES
 -- Estructura de tabla para la tabla `modulo`
 --
 
-CREATE TABLE `modulo` (
-  `codModulo` int(11) NOT NULL COMMENT 'Codigo de modulo',
+DROP TABLE IF EXISTS `modulo`;
+CREATE TABLE IF NOT EXISTS `modulo` (
+  `codModulo` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de modulo',
   `nombre` varchar(150) NOT NULL COMMENT 'Nombre del modulo',
   `uFormativa` varchar(50) NOT NULL COMMENT 'Unidad formativa del modulo',
-  `duracion` int(11) NOT NULL COMMENT 'Duracion del modulo'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `duracion` int(11) NOT NULL COMMENT 'Duracion del modulo',
+  PRIMARY KEY (`codModulo`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `modulo`
+--
+
+TRUNCATE TABLE `modulo`;
 -- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `tipocurso`
 --
 
-CREATE TABLE `tipocurso` (
-  `codTipoCurso` int(11) NOT NULL COMMENT 'Codigo del tipo de curso',
-  `nombre` varchar(50) NOT NULL COMMENT 'Nombre del tipo de curso'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `tipocurso`;
+CREATE TABLE IF NOT EXISTS `tipocurso` (
+  `codTipoCurso` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo del tipo de curso',
+  `nombre` varchar(50) NOT NULL COMMENT 'Nombre del tipo de curso',
+  PRIMARY KEY (`codTipoCurso`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
+--
+-- Truncar tablas antes de insertar `tipocurso`
+--
+
+TRUNCATE TABLE `tipocurso`;
 --
 -- Volcado de datos para la tabla `tipocurso`
 --
@@ -208,90 +284,6 @@ INSERT INTO `tipocurso` (`codTipoCurso`, `nombre`) VALUES
 (2, 'Hobetuz'),
 (3, 'Fundacion Tripartita');
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `alumno`
---
-ALTER TABLE `alumno`
-  ADD PRIMARY KEY (`codAlumno`),
-  ADD UNIQUE KEY `dni-nie` (`dni_nie`),
-  ADD UNIQUE KEY `codGenero_2` (`codGenero`),
-  ADD KEY `codGenero` (`codGenero`);
-
---
--- Indices de la tabla `calificacion`
---
-ALTER TABLE `calificacion`
-  ADD PRIMARY KEY (`codAlumno`,`codCurso`,`codModulo`),
-  ADD KEY `fk_calificacion_curso` (`codCurso`),
-  ADD KEY `fk_calificacion_modulo` (`codModulo`);
-
---
--- Indices de la tabla `curso`
---
-ALTER TABLE `curso`
-  ADD PRIMARY KEY (`codCurso`),
-  ADD UNIQUE KEY `codTipoCurso` (`codTipoCurso`),
-  ADD KEY `codTipoCurso_2` (`codTipoCurso`);
-
---
--- Indices de la tabla `curso_alumno`
---
-ALTER TABLE `curso_alumno`
-  ADD PRIMARY KEY (`codAlumno`,`codCurso`),
-  ADD UNIQUE KEY `referencia` (`referencia`),
-  ADD KEY `fk_cursoAlumno_curso` (`codCurso`);
-
---
--- Indices de la tabla `genero`
---
-ALTER TABLE `genero`
-  ADD PRIMARY KEY (`codGenero`);
-
---
--- Indices de la tabla `modulo`
---
-ALTER TABLE `modulo`
-  ADD PRIMARY KEY (`codModulo`);
-
---
--- Indices de la tabla `tipocurso`
---
-ALTER TABLE `tipocurso`
-  ADD PRIMARY KEY (`codTipoCurso`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `alumno`
---
-ALTER TABLE `alumno`
-  MODIFY `codAlumno` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Indice del alumno', AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `curso`
---
-ALTER TABLE `curso`
-  MODIFY `codCurso` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de curso', AUTO_INCREMENT=9;
---
--- AUTO_INCREMENT de la tabla `genero`
---
-ALTER TABLE `genero`
-  MODIFY `codGenero` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de genero', AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `modulo`
---
-ALTER TABLE `modulo`
-  MODIFY `codModulo` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo de modulo', AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `tipocurso`
---
-ALTER TABLE `tipocurso`
-  MODIFY `codTipoCurso` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Codigo del tipo de curso', AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
